845e1ad8e9002324089450ff69c3be386c8e110a
paul <paul@frozenphoenix.net>
2018-12-05 12:13:32 -0500

    desktop

---
+++ desktop/openbox/autostart.sh
@@ -0,0 +1,6 @@
+# .$GLOBALAUTOSTART &
+xrandr --auto --output DFP2 --right-of DFP1
+lxpanel &
+xscreensaver -no-splash &
+#synergy &
+feh --bg-scale /home/paul/desktop_img_0.jpg /home/paul/desktop_img_1.jpg
