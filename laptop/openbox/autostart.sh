a78a42c5129db68938e2e715097e752e10901ac1
obi11235 <paul@frozenphoenix.net>
2018-12-05 12:15:39 -0500

    laptop

---
+++ laptop/openbox/autostart.sh
@@ -0,0 +1,7 @@
+# .$GLOBALAUTOSTART &
+/home/paul/.config/openbox/local_autostart
+
+lxpanel &
+xscreensaver -no-splash &
+synergy &
+feh --bg-scale /home/paul/desktop_img_0.jpg /home/paul/desktop_img_1.jpg
